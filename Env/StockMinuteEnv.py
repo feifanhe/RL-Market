a3a7de8e3f077900330bd55397e313b4d87a5642
Fei-fan He <feifanho@gmail.com>
2019-08-19 11:45:48 +0800

    Update StockMinuteEnv.py

--- Env/StockMinuteEnv.py
+++ Env/StockMinuteEnv.py
@@ -11,10 +11,9 @@ from collections import deque
 
 #%%
 class Env():
-    def __init__(self,
-                  stock_folder, #股票所在資料夾
-                  ):
-
+    def __init__(
+            self,
+            stock_folder):
         # Env parameter initial
         self.stock_folder = stock_folder
         self.enable_fee = False
@@ -24,67 +23,39 @@ class Env():
     FEE_RATE = 0.001425
     FEE_MIN = 20
     
-    # 讀取台股交易日
-    def load_trading_day(self):
-        df = pd.read_excel(self.stock_folder + 'Y9999.xlsx')
-        start_date_row = df.loc[df['年月日'] == self.start_date]
-        assert len(start_date_row) > 0, '起始日無交易'
-        start_date_index = start_date_row.index[0]
-        assert start_date_index >= self.history_steps, '交易日資料不足'
-        head_date_index = start_date_index - self.history_steps
-        end_date_index = start_date_index + self.steps
-        self.trading_day = pd.DatetimeIndex(df['年月日'].iloc[head_date_index:end_date_index])
-        
     # 讀取個股資料
     def load_target_price(self):
-        open_price = pd.DataFrame(index=self.trading_day, columns=self.stock_targets)
-        close_price = pd.DataFrame(index=self.trading_day, columns=self.stock_targets)
+        open_price = pd.DataFrame(columns=self.stock_targets)
+        close_price = pd.DataFrame(columns=self.stock_targets)
         
         for i in range(self.stock_targets_count):
             # create table
+            global df
             df = pd.DataFrame([])
             
-            # handle cross-year action
-            for year in range(self.trading_day[0].year, self.trading_day[-1].year + 1):
-                filename = "{}{}/{}.xlsx".format(self.stock_folder, self.stock_targets[i], year)
-                df_tmp = pd.read_excel(filename, index_col = 0)
-                df = df.append(df_tmp)
-                
-            # set date as index
+            filename = '%s%s/%d/%02d.xlsx' % (self.stock_folder, self.stock_targets[i], self.start_date.year, self.start_date.month)
+            col_type = {
+                    'Date': np.str,
+                    'Time': np.str,
+                    'Open': np.float,
+                    'High': np.float,
+                    'Low': np.float,
+                    'Close': np.float,
+                    'Vol': np.int,
+                    'Pattern': np.int,
+                    'IsReal': np.int,
+                    }
+            df = pd.read_excel(filename, index_col = 0, dtype = col_type)
+            df = df[df['Date'] == '%d%02d%02d' % (self.start_date.year, self.start_date.month, self.start_date.day)]
             df = df.reset_index().drop(['index'], axis=1)
-            df['Date'] = pd.to_datetime(df['Date'], format='%Y%m%d')
-            df = df.set_index('Date')
-            
-            # handle missing prices
-            df_slice =  df.loc[self.trading_day, ['Open', 'Close']]
-            for index, row in pd.isnull(df_slice).iterrows():
-                if row['Open']:
-                    df_slice.loc[index, 'Open'] = df.loc[:index, 'Close'].iloc[-1]
-                if row['Close']:
-                    df_slice.loc[index, 'Close'] = df.loc[:index, 'Close'].iloc[-1]
-            
+           
             # update table
-            open_price[self.stock_targets[i]] = df_slice['Open']
-            close_price[self.stock_targets[i]] = df_slice['Close']
+            open_price[self.stock_targets[i]] = df['Open']
+            close_price[self.stock_targets[i]] = df['Close']
         
         self.open = open_price.values
         self.close = close_price.values
         
-    # 讀取除息資料    
-    def load_target_dividend(self): 
-        df = pd.read_csv(self.stock_folder + 'dividend.csv')
-        df['年月日'] = pd.to_datetime(df['年月日'])
-        df = df.set_index('年月日')
-        
-        # create table
-        self.dividend = pd.DataFrame(
-                data = 0,
-                index=self.trading_day, 
-                columns=self.stock_targets)
-        
-        # update table with dividend data from intersection of data cloumns and stock targets
-        self.dividend[df.columns & self.stock_targets] = df[df.columns & self.stock_targets]
-            
     def reset(
             self,
             cash, #初始化資金
@@ -93,22 +64,22 @@ class Env():
             history_steps, #窺視歷史價格天數
             stock_targets, #要進行資產配置的股票
             ):
-        
+
         self.cnt = 0
         self.cash = int(cash)
-        self.start_date = start_date
+        self.start_date = pd.to_datetime(start_date)
         self.steps = steps
         self.history_steps = history_steps
+        
+        assert self.steps + self.history_steps < 270, '一日交易270分鐘'
+        
         self.stock_targets = stock_targets
         self.stock_targets_count = len(stock_targets)
         self.position = np.zeros(self.stock_targets_count, dtype = int)
         self.avg_cost = np.zeros(self.stock_targets_count)
         self.cost_queue = [deque([]) for _ in range(self.stock_targets_count)]
         
-        self.load_trading_day()
         self.load_target_price()
-        self.load_target_dividend()
-        
         self.stock_targets_idx = {j:i for i, j in enumerate(self.stock_targets)}
         
         return (self.close[:self.history_steps], # 歷史收盤價
@@ -162,7 +133,7 @@ class Env():
         
         # 修正持有部位和平均成本
         self.position[cond_sell] += order[cond_sell]
-        self.avg_cost[(self.position) == 0] = 0
+        #self.avg_cost[(self.position) == 0] = 0
         
         return total_income, profit
     
@@ -189,9 +160,9 @@ class Env():
         cond_buy = (order > 0)
         
         # 買進時會累加交易成本
-        self.avg_cost[cond_buy] = (
+        """self.avg_cost[cond_buy] = (
                 (self.avg_cost * self.position + open_price * order)[cond_buy]
-                / (self.position + order)[cond_buy])
+                / (self.position + order)[cond_buy])"""
         
         total_cost = 0
         # append to cost queue
@@ -202,12 +173,21 @@ class Env():
                 cost += int(open_price[i] * 1000)
             cost += self.get_fee(cost)
             total_cost += cost
-            
+            print( sum(self.cost_queue[i]), len(self.cost_queue[i]))
+        
         self.position[cond_buy] += order[cond_buy]
+        
+        # average cost
+        for i in range(self.stock_targets_count):
+            if self.position[i] == 0:
+                self.avg_cost[i] = 0
+                continue
+            self.avg_cost[i] = sum(self.cost_queue[i]) / self.position[i]
+        
         return total_cost
     
     def step(self, action):
-        date_index = self.history_steps + self.cnt
+        time_index = self.history_steps + self.cnt
         
         # 委託單
         order = np.zeros(self.stock_targets_count, dtype = int)
@@ -217,25 +197,21 @@ class Env():
         order_deal = order.copy()
 
         # sell
-        income, profit = self.__sell(order_deal, self.open[date_index])
+        income, profit = self.__sell(order_deal, self.open[time_index])
         self.cash += income
         
         # buy
-        cost = self.__buy(order_deal, self.open[date_index])
+        cost = self.__buy(order_deal, self.open[time_index])
         self.cash -= cost
         
         # 未實現損益
-        unrealized = int(np.sum((self.close[date_index] - self.avg_cost) * self.position * 1000))
-        
-        # 計算配息
-        profit_dividend = np.sum(self.dividend.iloc[date_index] * self.position * 1000)
-        profit += profit_dividend
+        unrealized = int(np.sum((self.close[time_index] - self.avg_cost) * self.position * 1000))
         
         self.cnt += 1
         if self.cnt == self.steps:
             self.done = True
         
-        return (self.close[self.cnt:date_index], # 歷史收盤價
+        return (self.close[self.cnt:time_index], # 歷史收盤價
                 self.cash, # 剩餘現金
                 unrealized, # 未實現資產市值
                 profit, # 損益
@@ -245,7 +221,7 @@ class Env():
                 self.position)
 
 if __name__ == '__main__':
-    env = Env('./stock_data/')
+    env = Env('./stock_data_min_mon/')
     
     cash = int(1e+5)
     start_date = '2016/01/29'
@@ -272,20 +248,6 @@ if __name__ == '__main__':
              [],
              [],
             ]   
-    """
-    actions = [
-             [['1101', 1], ['2330', 10]],
-             [['1301', 1], ['2330', 2]],
-             [['1301', 3], ['2330', -1]],
-             [['1101', 5], ['1301', -20]],
-             [],
-             [],
-             [],
-             [],
-             [],
-             [],
-            ]
-    """
     
     for i in range(steps):
         print('[step %d]' % (i + 1))
@@ -300,3 +262,4 @@ if __name__ == '__main__':
         print('%s\t%s\t%s' % ('cash', 'profit', 'unrealized'))
         print('%d\t%d\t%d' % (cash, profit, unrealized))
         print()
+        
